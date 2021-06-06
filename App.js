569d913cb1cb723ae0de1ef5762504b96a50b802
LuizDaniloRibeiro <danilospfcribeiro@gmail.com>
2021-06-06 17:01:22 -0300

    instalação dependencias

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá</Text>
+      <Text>Olá, mundo!</Text>
       <StatusBar style="auto" />
     </View>
   );
